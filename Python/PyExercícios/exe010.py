e4993d84603cf679cdcb7474fcfd8d716430451f
Fábio Santos <fabio.rsp2012@gmail.com>
2020-05-02 17:48:29 -0300

    Fiz pequenas mudanças em um código e nome da pasta

--- Python/PyExercícios/exe010.py
+++ Python/PyExercícios/exe010.py
@@ -1,2 +1,2 @@
 r = float(input('Quanto de dinheiro você tem na carteira? R$'))
-(print(f'Com R${r:.2f} você pode comprar US${r/5.49:.2f} e EUR€{r/6.09:.2F}'))
+(print(f'Com R${r:.2f} você pode comprar US${r/5.49:.2f}'))
