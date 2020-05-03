6a24bf7e7f99cd825bc3521800f187dad8eb3a71
Fábio Santos <fabio.rsp2012@gmail.com>
2020-05-03 18:12:16 -0300

    Create exe016.py

---
+++ Estudos de Python/PyExercícios/exe016.py
@@ -0,0 +1,11 @@
+from math import trunc
+num = float(input('Digite um número: '))
+print(f'O número inteiro é {trunc(num)}')
+
+#num = float(input('Digite um número: '))
+#print(f'O número inteiro é {int(num)}')
+
+#import math
+#num = float(input('Digite um número: '))
+#num2 = math.trunc(num)
+#print(f'O número inteiro é {num2}')
