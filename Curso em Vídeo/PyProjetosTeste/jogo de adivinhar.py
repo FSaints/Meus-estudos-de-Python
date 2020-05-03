3bbf916c6c4c1df53cf37b8e004b355883158c28
Fábio Santos <fabio.rsp2012@gmail.com>
2020-05-03 19:07:02 -0300

    Update jogo de adivinhar.py

--- Curso em Vídeo/PyProjetosTeste/jogo de adivinhar.py
+++ Curso em Vídeo/PyProjetosTeste/jogo de adivinhar.py
@@ -1,6 +1,6 @@
 from random import randint
 while True:
-    num1 = int(input('Tente acertar um número um número de 0 a 10: '))
+    num1 = int(input('Tente acertar um número de 0 a 10: '))
     if num1 == randint(0, 10):
         print('PARABÉNS, VOCÊ ACERTOU!!')
     else:
